04e54e48df4b6fd12d61e41904b84d688776fed4
tharubijay <bijaytharu306@gmail.com>
2022-11-27 12:34:23 +0545

    category and product-detail

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('', HomeView.as_view(), name = 'home'),
     path('category/<slug>', CategoryView.as_view(), name = 'category'),
     path('product_detail/<slug>', ProductDetailView.as_view(), name = 'product_detail'),
-    path('product_review', product_review, name='product_review'),     
+    path('product_review', product_review, name='product_review'),   
+    path('search_product', SearchProduct.as_view(), name='search_product'),  
 ]
 
 
